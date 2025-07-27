1c95c089cc90cfeab2a75c6a4fd1492cc321f494
Nu11 <86795298+Nu11ified@users.noreply.github.com>
2025-07-26 21:32:25 -0500

    Add circles to grid sections in JUI draw method
    Each grid section now draws a circle at its center with a radius based on section size, using the Slateblue color. This enhances the visual appearance of the grid.

--- src/ui/jui.cpp
+++ src/ui/jui.cpp
@@ -12,11 +12,19 @@ void JUI::Jui::draw() {
   int num_sec_x = GetScreenWidth() / sec_size;
   int num_sec_y = GetScreenHeight() / sec_size;
 
+  float radius = sec_size / 2.0;
+
   for (size_t x = 0; x < num_sec_x; x++) {
     auto line_color = to_ray_color(Jcolor::Cornsilk);
     DrawLine(x * sec_size, 0, x * sec_size, 
       GetScreenHeight(), line_color);
     for (size_t y = 0; y < num_sec_y; y++) {
+
+      DrawCircle((x * sec_size) + radius, y * sec_size + radius,
+        radius, to_ray_color(Jcolor::Slateblue));
+
+      DrawLine(0, y * sec_size, GetScreenWidth(), y * sec_size,
+        line_color);
     }
   }
 };
